9769436e8e23046d4fb3e87775df6f53bc316ef9
WangKe <1046653233@qq.com>
2019-09-15 14:24:01 +0800

    clone修改

--- gitdemo/src/com/lechi/firstClass.java
+++ gitdemo/src/com/lechi/firstClass.java
@@ -24,7 +24,7 @@ public class firstClass {
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
+        System.out.println("hello git");
     }
 
 }
