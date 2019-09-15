f05f28042b04c23a12fdb2b50d15630ede4df42b
WangKe <1046653233@qq.com>
2019-09-15 14:35:18 +0800

    添加test2

---
+++ gitdemo/src/com/lechi/test2.java
@@ -0,0 +1,16 @@
+package com.lechi;
+
+/**
+ * @ClassName test2
+ * @Description ToDo
+ * @Author WangKe
+ * @Date 2019/9/15 14:34
+ * @Version 1.0
+ **/
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("dev2");
+        System.out.println("dev2");
+    }
+}
