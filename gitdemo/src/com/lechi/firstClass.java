b4ad802875475d25d200a05c5f752325e5709d10
WangKe <1046653233@qq.com>
2019-09-15 13:59:15 +0800

    修改

---
+++ gitdemo/src/com/lechi/firstClass.java
@@ -0,0 +1,25 @@
+package com.lechi;
+
+/**
+ * @ClassName firstClas
+ * @Description ToDo
+ * @Author WangKe
+ * @Date 2019/9/15 13:50
+ * @Version 1.0
+ **/
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+
+    public void test(){
+        int i = 0;
+        i +=10;
+        System.out.println();
+    }
+
+}
